a49ae160be0c2704e1bac53c8837792487159926
Patrick Galbraith <patg@patg.net>
2010-01-08 09:15:37 -0800

    added "include" directory to Makefile.mingw

--- win32/config.h
+++ win32/config.h
@@ -12,22 +12,22 @@
 #define PACKAGE_NAME "memcached"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "memcached 1.4.4-34-g61c7096"
+#define PACKAGE_STRING "memcached 1.4.4-101-gd87daef"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_DESCRIPTION "memcached 1.4.4-34-g61c7096 is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load. Win32 port by Kronuz."
+#define PACKAGE_DESCRIPTION "memcached 1.4.4-101-gd87daef is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load. Win32 port by Kronuz."
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "memcached"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.4-34-g61c7096"
+#define PACKAGE_VERSION "1.4.4-101-gd87daef"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.4.4-34-g61c7096"
+#define VERSION "1.4.4-101-gd87daef"
 
 /* machine is littleendian */
 #define ENDIAN_LITTLE 1
