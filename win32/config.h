8f13e8d6dc43bcacbbab5b043be2592d4ce1d7fe
Patrick Galbraith <patg@patg.net>
2010-01-14 00:49:37 +0000

    Successful compile of default_engine.dll

--- win32/config.h
+++ win32/config.h
@@ -12,22 +12,22 @@
 #define PACKAGE_NAME "memcached"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "memcached 1.4.4-104-gea77b61"
+#define PACKAGE_STRING "memcached 1.4.4-107-g5fbc52c"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_DESCRIPTION "memcached 1.4.4-104-gea77b61 is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load. Win32 port by Kronuz."
+#define PACKAGE_DESCRIPTION "memcached 1.4.4-107-g5fbc52c is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load. Win32 port by Kronuz."
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "memcached"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.4-104-gea77b61"
+#define PACKAGE_VERSION "1.4.4-107-g5fbc52c"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.4.4-104-gea77b61"
+#define VERSION "1.4.4-107-g5fbc52c"
 
 /* machine is littleendian */
 #define ENDIAN_LITTLE 1
