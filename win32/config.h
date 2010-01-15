7ae0679876d87676dc731fd600f1f4b5ccc20dce
Trond Norbye <Trond.Norbye@gmail.com>
2010-01-14 16:48:10 -0800

    Removed use of perl to build on win32

--- win32/config.h
+++ win32/config.h
@@ -1,4 +1,3 @@
-/* Name of package */
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
@@ -6,28 +5,21 @@
 #define PACKAGE "memcached"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "patrick.galbraith@northscale.com"
+#define PACKAGE_BUGREPORT "patrick.galbraith\@northscale.com"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "memcached"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "memcached 1.4.4-107-g5fbc52c"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_DESCRIPTION "memcached 1.4.4-107-g5fbc52c is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load. Win32 port by Kronuz."
+#define PACKAGE_DESCRIPTION "memcached is a high-performance, distributed memory object caching system, generic in nature, but intended for use in speeding up dynamic web applications by alleviating database load."
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "memcached"
 
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.4-107-g5fbc52c"
+#include "config_version.h"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
-
-/* Version number of package */
-#define VERSION "1.4.4-107-g5fbc52c"
 
 /* machine is littleendian */
 #define ENDIAN_LITTLE 1
